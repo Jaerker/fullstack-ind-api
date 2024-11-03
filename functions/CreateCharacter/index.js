522bf14b213a53c03d0c1a2dcbfea59cb4ef5924
Jaerker <johanjakberger@gmail.com>
2024-11-03 10:46:59 +0100

    Added character creation

--- functions/CreateCharacter/index.js
+++ functions/CreateCharacter/index.js
@@ -1,6 +1,23 @@
 const response = require('../../responses/index').default;
 
 exports.handler = async (event) => {
+  const name = event.queryStringParameters?.name === undefined ? 'John Doe' : event.queryStringParameters.name;
+  const baseStats= {
+    strength: 2,
+    hp: 1,
+    speed: 2,
+    defense:1
+  }
+
+  const character = {
+    name: name,
+    race: 'Human',
+    strength: Math.ceil(Math.random() * 5) + baseStats.strength,
+    hp: (Math.ceil(Math.random() * 3) + baseStats.hp)*10,
+    speed: Math.ceil(Math.random() * 3) + baseStats.speed,
+    defense: Math.ceil(Math.random() * 5)+baseStats.defense
+  }
+
   
-  return response(200, 'This works!');
+  return response(200, character);
 };
